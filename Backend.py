d2e25490f6d07dd5dc29b68c936979bcc1a5ce28
Pedro Pérez <3008387860101@ingenieria.usac.edu.gt>
2021-04-14 14:11:49 -0600

    Backend del proyecto

--- Backend.py
+++ Backend.py
@@ -20,6 +20,7 @@ def registro_doctores():
         datos_doctores.append(data)
         print(datos_doctores)
         return jsonify({"mensaje":"datos ingresados"})
+
     if not datos_doctores and not datos_enfermeros:
         for i in range (len(datos_pacientes)):
              if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"]:
@@ -38,6 +39,36 @@ def registro_doctores():
                  datos_doctores.append(data)
                  print(datos_doctores)
                  return jsonify({"mensaje":"datos ingresados"})
+
+    if not datos_pacientes:
+        for i in range (len(datos_enfermeros)):
+            for j in range (len(datos_doctores)):
+                if data["nombre de usuario"] != datos_enfermeros[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"]:
+                    datos_doctores.append(data)
+                    print(datos_doctores)
+                    return jsonify({"mensaje":"datos ingresados"})
+    if not datos_doctores:
+        for i in range (len(datos_enfermeros)):
+            for j in range (len(datos_pacientes)):
+                if data["nombre de usuario"] != datos_enfermeros[i]["nombre de usuario"] and data["nombre de usuario"] != datos_pacientes[j]["nombre de usuario"]:
+                    datos_doctores.append(data)
+                    print(datos_doctores)
+                    return jsonify({"mensaje":"datos ingresados"})
+    if not datos_enfermeros:
+        for i in range (len(datos_pacientes)):
+            for j in range (len(datos_doctores)):
+                if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"]:
+                    datos_doctores.append(data)
+                    print(datos_doctores)
+                    return jsonify({"mensaje":"datos ingresados"})
+
+    for i in range (len(datos_pacientes)):
+        for j in range (len(datos_doctores)):
+            for k in range (len(datos_enfermeros)):
+                if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"] and data["nombre de usuario"] != datos_enfermeros[k]["nombre de usuario"]:
+                    datos_doctores.append(data)
+                    print(datos_doctores)
+                    return jsonify({"mensaje":"datos ingresados"})
     return jsonify({"mensaje":"El nombre de usuario ya existe"})
 
 @app.route('/registropacientes', methods=['POST'])
@@ -47,6 +78,7 @@ def registro_pacientes():
         datos_pacientes.append(data)
         print(datos_pacientes)
         return jsonify({"mensaje":"datos ingresados"})
+
     if not datos_doctores and not datos_enfermeros:
         for i in range (len(datos_pacientes)):
              if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"]:
@@ -65,6 +97,94 @@ def registro_pacientes():
                  datos_pacientes.append(data)
                  print(datos_pacientes)
                  return jsonify({"mensaje":"datos ingresados"})
+
+    if not datos_pacientes:
+        for i in range (len(datos_enfermeros)):
+            for j in range (len(datos_doctores)):
+                if data["nombre de usuario"] != datos_enfermeros[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"]:
+                    datos_pacientes.append(data)
+                    print(datos_pacientes)
+                    return jsonify({"mensaje":"datos ingresados"})
+    if not datos_doctores:
+        for i in range (len(datos_enfermeros)):
+            for j in range (len(datos_pacientes)):
+                if data["nombre de usuario"] != datos_enfermeros[i]["nombre de usuario"] and data["nombre de usuario"] != datos_pacientes[j]["nombre de usuario"]:
+                    datos_pacientes.append(data)
+                    print(datos_pacientes)
+                    return jsonify({"mensaje":"datos ingresados"})
+    if not datos_enfermeros:
+        for i in range (len(datos_pacientes)):
+            for j in range (len(datos_doctores)):
+                if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"]:
+                    datos_pacientes.append(data)
+                    print(datos_pacientes)
+                    return jsonify({"mensaje":"datos ingresados"})
+
+    for i in range (len(datos_pacientes)):
+        for j in range (len(datos_doctores)):
+            for k in range (len(datos_enfermeros)):
+                if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"] and data["nombre de usuario"] != datos_enfermeros[k]["nombre de usuario"]:
+                    datos_pacientes.append(data)
+                    print(datos_pacientes)
+                    return jsonify({"mensaje":"datos ingresados"})
+    return jsonify({"mensaje":"El nombre de usuario ya existe"})
+
+@app.route('/registroenfermeros', methods=['POST'])
+def registro_enfermeros():
+    data = request.get_json(force=True)
+    if not datos_doctores and not datos_enfermeros and not datos_pacientes:
+        datos_enfermeros.append(data)
+        print(datos_enfermeros)
+        return jsonify({"mensaje":"datos ingresados"})
+
+    if not datos_doctores and not datos_enfermeros:
+        for i in range (len(datos_pacientes)):
+             if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"]:
+                 datos_enfermeros.append(data)
+                 print(datos_enfermeros)
+                 return jsonify({"mensaje":"datos ingresados"})
+    if not datos_pacientes and not datos_enfermeros:
+        for i in range (len(datos_doctores)):
+             if data["nombre de usuario"] != datos_doctores[i]["nombre de usuario"]:
+                 datos_enfermeros.append(data)
+                 print(datos_enfermeros)
+                 return jsonify({"mensaje":"datos ingresados"})
+    if not datos_pacientes and not datos_doctores:
+        for i in range (len(datos_enfermeros)):
+             if data["nombre de usuario"] != datos_enfermeros[i]["nombre de usuario"]:
+                 datos_enfermeros.append(data)
+                 print(datos_enfermeros)
+                 return jsonify({"mensaje":"datos ingresados"})
+
+    if not datos_pacientes:
+        for i in range (len(datos_enfermeros)):
+            for j in range (len(datos_doctores)):
+                if data["nombre de usuario"] != datos_enfermeros[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"]:
+                    datos_enfermeros.append(data)
+                    print(datos_enfermeros)
+                    return jsonify({"mensaje":"datos ingresados"})
+    if not datos_doctores:
+        for i in range (len(datos_enfermeros)):
+            for j in range (len(datos_pacientes)):
+                if data["nombre de usuario"] != datos_enfermeros[i]["nombre de usuario"] and data["nombre de usuario"] != datos_pacientes[j]["nombre de usuario"]:
+                    datos_enfermeros.append(data)
+                    print(datos_enfermeros)
+                    return jsonify({"mensaje":"datos ingresados"})
+    if not datos_enfermeros:
+        for i in range (len(datos_pacientes)):
+            for j in range (len(datos_doctores)):
+                if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"]:
+                    datos_enfermeros.append(data)
+                    print(datos_enfermeros)
+                    return jsonify({"mensaje":"datos ingresados"})
+
+    for i in range (len(datos_pacientes)):
+        for j in range (len(datos_doctores)):
+            for k in range (len(datos_enfermeros)):
+                if data["nombre de usuario"] != datos_pacientes[i]["nombre de usuario"] and data["nombre de usuario"] != datos_doctores[j]["nombre de usuario"] and data["nombre de usuario"] != datos_enfermeros[k]["nombre de usuario"]:
+                    datos_enfermeros.append(data)
+                    print(datos_enfermeros)
+                    return jsonify({"mensaje":"datos ingresados"})
     return jsonify({"mensaje":"El nombre de usuario ya existe"})
 
 @app.route('/datospacientes')
@@ -74,6 +194,10 @@ def pacientes():
 @app.route('/datosdoctores')
 def doctores():
     return jsonify({"usuarios": datos_doctores, "titulo":"Lista de doctores"})
+
+@app.route('/datosenfermeros')
+def enfermeros():
+    return jsonify({"usuarios": datos_enfermeros, "titulo":"Lista de enfermeros"})
 
 @app.route('/login', methods = ['POST'])
 def login():
